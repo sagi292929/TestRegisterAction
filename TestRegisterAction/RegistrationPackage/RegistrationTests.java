a0fee8b47e71e0bd533163836126a61c4500375d
LLeshem <LLeshem@DESKTOP-QIFRP5P>
2019-08-21 20:02:05 +0300

    Update 1

--- TestRegisterAction/RegistrationPackage/RegistrationTests.java
+++ TestRegisterAction/RegistrationPackage/RegistrationTests.java
@@ -2,7 +2,13 @@ package RegistrationPackage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -16,87 +22,110 @@ import jdk.jfr.Description;
 
 public class RegistrationTests {
 	
+	private String requestUrl = "https://rest-eus1.ott.kaltura.com/restful_v5_0/api_v3/service/ottuser/action/register";
+	
+	
+	@Test
+	@Description(value = "New user Registration") 
+	public void test_sanityByObject() throws JsonGenerationException, JsonMappingException, IOException {
+		
+		String rndUserName = GetRandomUserName();
+		
+		Payload payload = new Payload();
+		payload.partnerId = 185;
+		payload.password = "password";
+		
+		User user = new User();
+		user.objectType = "KalturaOTTUser";
+		user.username = rndUserName + "1537875168493";
+		user.firstName = GetRandomUserName();
+		user.lastName = "1537875168493";
+		user.email = "qaott2018+NEW_USER1537875168493@gmail.com";
+		user.address = "NEW_USER fake address";
+		user.city = "NEW_USER fake city";
+		user.countryId = 7;
+		user.externalId = rndUserName + "1537875168493";
+		
+		payload.user = user;
+		
+		String res = SendPayloadAndGetResponse(payload);
+		
+		//Note - I've tried to use the response as object but it was inconsistent (I'll explain more by mail)
+	//	Response response = GetResponse(res);
+	//	assertTrue(response.result.username == rndUserName);
+		
+		assertTrue(res.contains(rndUserName));
+		assertFalse(res.contains("error"));
+	}
+	
+
+	
 	@Test
 	@Description(value = "Negative test - User exists") 
-	public void test_Negative1() {
+	public void test_Negative1ByObject() throws JsonGenerationException, JsonMappingException, IOException 
+	{
+		Payload payload = new Payload();
+		payload.partnerId = 185;
+		payload.password = "password";
 		
-		String requestUrl = "https://rest-eus1.ott.kaltura.com/restful_v5_0/api_v3/service/ottuser/action/register";
-		String payload = "{\r\n" + 
-				"  \"partnerId\": 185,\r\n" + 
-				"  \"user\": {\r\n" + 
-				"    \"objectType\": \"KalturaOTTUser\",\r\n" + 
-				"    \"username\": \"sameUser\",\r\n" + 
-				"    \"firstName\": \"dfdee\",\r\n" + 
-				"    \"lastName\": \"1537875168493\",\r\n" + 
-				"    \"email\": \"qaott2018+sameUser@gmail.com\",\r\n" + 
-				"    \"address\": \"dfdee fake address\",\r\n" + 
-				"    \"city\": \"dfdee fake city\",\r\n" + 
-				"    \"countryId\": 7,\r\n" + 
-				"    \"externalId\": \"sameUser\"\r\n" + 
-				"  },\r\n" + 
-				"  \"password\": \"password\"\r\n" + 
-				"}\r\n";
+		User user = new User();
+		user.objectType = "KalturaOTTUser";
+		user.username = "sameUser";
+		user.firstName = "dfdee";
+		user.lastName = "1537875168493";
+		user.email = "qaott2018+sameUser@gmail.com";
+		user.address = "dfdee fake address";
+		user.city = "dfdee fake city";
+		user.countryId = 7;
+		user.externalId = "sameUser1537875168493";
 		
-		sendPostRequest(requestUrl, payload);
+		payload.user = user;
 		
-		String res = sendPostRequest(requestUrl, payload);
+		String res = SendPayloadAndGetResponse(payload);
 		
-		assertTrue(res.contains("\"code\": \"2014\""));
+		assertTrue(res.contains("\"code\": \"2014\""), res);
 		assertTrue(res.contains("error"));
 		assertTrue(res.contains("User exists"));
 	}
 	
-	
 	@Test
 	@Description(value = "Negative test - Missing parameter") 
-	public void test_Negative2() {
+	public void test_Negative2ByObject() throws JsonGenerationException, JsonMappingException, IOException {
 		
-		String requestUrl = "https://rest-eus1.ott.kaltura.com/restful_v5_0/api_v3/service/ottuser/action/register";
-		String payload = "{\"partnerId\": 185}";
+		Payload payload = new Payload();
+		payload.partnerId = 185;
+		payload.password = "password";
 		
-		sendPostRequest(requestUrl, payload);
-		
-		String res = sendPostRequest(requestUrl, payload);
-		
+		String res = SendPayloadAndGetResponse(payload);
+	
 		assertTrue(res.contains("Missing parameter [user]"));
 		assertTrue(res.contains("error"));
 		assertTrue(res.contains("\"code\": \"500013\""));
 	}
 	
 	
-	@Test
-	@Description(value = "New user Registration") 
-	public void test_sanity() {
+	
+	private Response GetResponse(String jsonRes) throws JsonParseException, JsonMappingException, IOException
+	{
+		ObjectMapper mapper = new ObjectMapper();
 		
-		String requestUrl = "https://rest-eus1.ott.kaltura.com/restful_v5_0/api_v3/service/ottuser/action/register";
-		String payload = "{\r\n" + 
-				"  \"partnerId\": 185,\r\n" + 
-				"  \"user\": {\r\n" + 
-				"    \"objectType\": \"KalturaOTTUser\",\r\n" + 
-				"    \"username\": \"NEW_USER1537875168493\",\r\n" + 
-				"    \"firstName\": \"NEW_USER\",\r\n" + 
-				"    \"lastName\": \"1537875168493\",\r\n" + 
-				"    \"email\": \"qaott2018+NEW_USER1537875168493@gmail.com\",\r\n" + 
-				"    \"address\": \"NEW_USER fake address\",\r\n" + 
-				"    \"city\": \"NEW_USER fake city\",\r\n" + 
-				"    \"countryId\": 7,\r\n" + 
-				"    \"externalId\": \"NEW_USER1537875168493\"\r\n" + 
-				"  },\r\n" + 
-				"  \"password\": \"password\"\r\n" + 
-				"}\r\n";
+		Response response = mapper.readValue(jsonRes, Response.class);
 		
-		//	String payload = "{  \"partnerId\": 185,  \"user\": {    \"objectType\": \"KalturaOTTUser\",    \"username\": \"dfdee1537875168491\",    \"firstName\": \"dfdee\",    \"lastName\": \"1537875168491\",    \"email\": \"qaott2018+dfdee1537875168491@gmail.com\",    \"address\": \"dfdee fake address\",    \"city\": \"dfdee fake city\",    \"countryId\": 7,    \"externalId\": \"dfdee1537875168491\"  },  \"password\": \"password\"}";
-		
-		String RndUserName = GetRandomUserName();
-		payload = payload.replace("NEW_USER", RndUserName);
-		
-		String res = sendPostRequest(requestUrl, payload);
-		
-		assertTrue(res.contains(RndUserName));
-		assertFalse(res.contains("error"));
+		return response;
 	}
 	
-	private static String sendPostRequest(String requestUrl, String jsonInputString) {
+	private String SendPayloadAndGetResponse(Payload payload) throws JsonGenerationException, JsonMappingException, IOException
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		
+		String payloadStr = mapper.writeValueAsString(payload);
+
+		String res = SendPostRequest(requestUrl, payloadStr);
+		
+		return res;
+	}
+	
+	private String SendPostRequest(String requestUrl, String jsonInputString) {
 	    try {
 	        URL url = new URL(requestUrl);
 	        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -112,7 +141,6 @@ public class RegistrationTests {
 	        writer.flush();
 	        writer.close();
 	        
-	      //display what returns the POST request
 	        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 	        StringBuffer jsonString = new StringBuffer();
 	        String line;
@@ -129,10 +157,15 @@ public class RegistrationTests {
 	
 	private String GetRandomUserName()
 	{
-		 byte[] array = new byte[7];
-		    new Random().nextBytes(array);
-		    String generatedString = new String(array, Charset.forName("UTF-8"));
-		    
-		    return generatedString;
+		RandomString rs = new RandomString(11);
+		
+		return rs.nextString();
+	
+		
+// byte[] array = new byte[7];
+//    new Random().nextBytes(array);
+//    String generatedString = new String(array, Charset.forName("UTF-8"));
+//    
+//    return generatedString;
 	}
 }
